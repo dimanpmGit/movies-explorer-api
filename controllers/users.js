39d6f79cc9897f3df5442ca4ed108b9fad15f25f
Dmitry Kontsevenko <dimanpm@yandex.ru>
2023-07-28 01:00:14 +0700

    Добавлена обработка ошибки обновления данных пользователя.

--- controllers/users.js
+++ controllers/users.js
@@ -40,6 +40,8 @@ const updateProfile = (req, res, next) => {
     .catch((err) => {
       if (err.code === 11000) {
         next(new ConflictError(MSG_USER_DUBLICATE));
+      } else if (err.name === 'ValidationError') {
+        next(new ValidationError(MSG_USER_WRONG_DATA));
       } else {
         next(err);
       }
